13ca59f1a176d642b51a3c544f16356779ee78fe
jenny-hub90 <jumbledjenny19@gmail.com>
2023-04-02 22:04:25 +0545

    added validation

--- Task1/todolist.js
+++ Task1/todolist.js
@@ -5,28 +5,28 @@ const todoList = document.querySelector('ul');
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault(); 
+
+  const title = titleInput.value.trim();
+  const task = taskInput.value.trim();
   
-  const title = titleInput.value;
-  const task = taskInput.value;
-  
+  if (title === '' || task === '') {
+    alert('Please enter a title and a task description.');
+    return;
+  }
   
   const listItem = document.createElement('li');
   const taskText = document.createTextNode(`${title}: ${task}`);
   listItem.appendChild(taskText);
 
-
   const deleteButton = document.createElement('button');
   deleteButton.innerHTML = 'Delete';
   listItem.appendChild(deleteButton);
-  
-  
+
   todoList.appendChild(listItem);
-  
-  
+
   titleInput.value = '';
   taskInput.value = '';
 });
-
 
 todoList.addEventListener('click', (e) => {
   if (e.target.nodeName === 'BUTTON') {
@@ -35,19 +35,23 @@ todoList.addEventListener('click', (e) => {
   }
 });
 
-
 function saveTodoList() {
-    localStorage.setItem('todoList', todoList.innerHTML);
-  }
-  submitButton.addEventListener('click', (e) => {
-    saveTodoList();
-  });
-  todoList.addEventListener('click', (e) => {
-    saveTodoList();
-  });
-  if (localStorage.getItem('todoList')) {
-    todoList.innerHTML = localStorage.getItem('todoList');
-  }
+  localStorage.setItem('todoList', todoList.innerHTML);
+}
+
+submitButton.addEventListener('click', (e) => {
+  saveTodoList();
+});
+
+todoList.addEventListener('click', (e) => {
+  saveTodoList();
+});
+
+if (localStorage.getItem('todoList')) {
+  todoList.innerHTML = localStorage.getItem('todoList');
+}
+
+
 
 
 
